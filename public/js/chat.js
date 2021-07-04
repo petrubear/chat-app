6d7332d9a4ab2b6f206b6872269cebb1df053ee1
petrubear <edison.martinez@gmail.com>
2021-07-04 13:30:18 -0500

    feat: button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,27 @@
 const socket = io();
 
+// elements
+const $messageForm = document.querySelector('#chat-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
     console.log(message);
 });
 
-document.querySelector('#chat-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // disable submit
+    $messageFormButton.setAttribute('disabled', 'disabled');
 
     // e.target es el form
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if (error) {
             return console.log(error);
         }
@@ -18,16 +30,19 @@ document.querySelector('#chat-form').addEventListener('submit', (e) => {
     });
 });
 
-document.querySelector('#send-location').addEventListener('click', (e) => {
+$sendLocationButton.addEventListener('click', (e) => {
     if (!navigator.geolocation) {
         return alert('Geolocation not available');
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled');
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         }, () => {
+            $sendLocationButton.removeAttribute('disabled');
             console.log('Location shared!');
         });
     });
