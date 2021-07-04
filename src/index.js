9d93f20ee6bd976f9bd5804070a7b59f1edf1a6e
petrubear <edison.martinez@gmail.com>
2021-07-04 12:29:01 -0500

    feat: user leaves

--- src/index.js
+++ src/index.js
@@ -13,9 +13,16 @@ app.use(express.static(staticFolder));
 
 io.on('connection', (socket) => {
     console.log('==> New websocket connection');
+
     socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
+
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', '==> user has left!');
     });
 });
 
