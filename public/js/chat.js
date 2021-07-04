1b36604c1c5b9ed8e5c4811d3f4ce0836c58b3b4
petrubear <edison.martinez@gmail.com>
2021-07-04 12:58:38 -0500

    feat: send user location

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector('#chat-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message);
 });
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not available');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        });
+    });
+});
